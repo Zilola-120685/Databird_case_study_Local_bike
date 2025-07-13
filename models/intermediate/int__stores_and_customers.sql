983dd7600098b46c3fdfd3587a5fbe2ee01e8da9
zilola.giyasova@gmail.com <zilola.giyasova@gmail.com>
2025-07-13 11:12:28 +0000

    feat(intermediate): stores and customers table added

---
+++ models/intermediate/int__stores_and_customers.sql
@@ -0,0 +1,16 @@
+SELECT 
+  o.store_id, 
+  o.staff_id,
+  MIN(o.order_date) AS first_order_date,
+  COUNT(o.order_id) AS number_of_orders,
+  COUNT(DISTINCT(o.customer_id)) AS number_of_customers,
+  ROUND(SUM(oi.net_order_item_amount),2) net_total_orders_amount,
+  ROUND(COUNT(o.order_id) * 1.0 / NULLIF(COUNT(DISTINCT o.customer_id), 0), 2) AS avg_number_orders_per_customer,
+  ROUND(SUM(oi.net_order_item_amount) / NULLIF(COUNT(o.order_id), 0), 2) AS avg_order_amount,
+  ROUND(SUM(oi.net_order_item_amount) / NULLIF(COUNT(DISTINCT o.customer_id), 0), 2) AS avg_order_amount_per_customer
+FROM {{ref('stg__orders')}} o
+LEFT JOIN {{ref('stg__order_items')}} oi
+  USING (order_id) 
+GROUP BY
+  store_id,
+  staff_id
